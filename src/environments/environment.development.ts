7908751caa7dff149be591c832b66cf4f7c7c66a
Mosut0 <mos.yas123@gmail.com>
2024-10-18 21:43:05 -0400

    Lab5

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase : {
+        apiKey: "AIzaSyAg3edVGrqwgpMgouxhjtEQGPbhLXytZXM",
+        authDomain: "seg3102-lab5-efa9d.firebaseapp.com",
+        projectId: "seg3102-lab5-efa9d",
+        storageBucket: "seg3102-lab5-efa9d.appspot.com",
+        messagingSenderId: "686290836105",
+        appId: "1:686290836105:web:affe202b50318ff9768458",
+        measurementId: "G-DSXJ6R41JB"
+    }
+};
